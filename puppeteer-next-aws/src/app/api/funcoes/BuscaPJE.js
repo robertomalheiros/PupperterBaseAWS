5dc4b52cfd22b1919732e87d9a32b71c5700dd01
robertomalheiros <robertomalheirosb@gmail.com>
2024-03-08 13:20:17 -0300

    Tentativa direto no aws-codigo antigo

--- puppeteer-next-aws/src/app/api/funcoes/BuscaPJE.js
+++ puppeteer-next-aws/src/app/api/funcoes/BuscaPJE.js
@@ -18,7 +18,7 @@ async function launchBrowser() {
         browser = await puppeteer.launch({
           args: chromium.args,
           defaultViewport: chromium.defaultViewport,
-          executablePath: await chromium.executablePath(),
+          executablePath: await chromium.executablePath,
           headless: chromium.headless,
           ignoreHTTPSErrors: true,
       });
